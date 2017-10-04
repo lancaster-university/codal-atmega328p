93687cc7a149fbf900c581c166d21af88663a885
James Devine <j.devine@lancaster.ac.uk>
2017-10-04 11:29:42 +0100

    removed headers, and added new functions for codal-core
    Due to the refactor, glue is no longer required between the core and the target.

--- ARDUINO_UNO/source/codal_target_hal.cpp
+++ ARDUINO_UNO/source/codal_target_hal.cpp
@@ -1,6 +1,7 @@
 #include "codal_target_hal.h"
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
+#include <util/delay.h>
 #include <avr/wdt.h>
 #include "ATMegaSerial.h"
 
@@ -25,6 +26,7 @@ void target_wait_for_event()
 
 void target_wait(uint32_t milliseconds)
 {
+    _delay_ms(milliseconds);
 }
 
 void target_reset()
@@ -45,3 +47,90 @@ void target_panic(int statusCode)
     }
     while (1) {}
 }
+
+struct PROCESSOR_TCB
+{
+    uint8_t SPLO;
+    uint8_t SPHI;
+
+    uint16_t LR;
+
+    uint8_t R20;
+    uint8_t R21;
+    uint8_t R22;
+    uint8_t R23;
+    uint8_t R24;
+    uint8_t R25;
+};
+
+PROCESSOR_WORD_TYPE fiber_initial_stack_base()
+{
+    return RAMEND - 0x02;;
+}
+
+void* tcb_allocate()
+{
+    return (void *)malloc(sizeof(PROCESSOR_TCB));
+}
+
+/**
+  * Configures the link register of the given tcb to have the value function.
+  *
+  * @param tcb The tcb to modify
+  * @param function the function the link register should point to.
+  */
+void tcb_configure_lr(void* tcb, PROCESSOR_WORD_TYPE function)
+{
+    PROCESSOR_TCB* tcbPointer = (PROCESSOR_TCB *)tcb;
+    tcbPointer-LR = function;
+}
+
+/**
+  * Configures the link register of the given tcb to have the value function.
+  *
+  * @param tcb The tcb to modify
+  * @param function the function the link register should point to.
+  */
+void tcb_configure_sp(void* tcb, PROCESSOR_WORD_TYPE sp)
+{
+    PROCESSOR_TCB* tcbPointer = (PROCESSOR_TCB *)tcb;
+    tcb->SPHI = codal::high(sp);
+    tcb->SPLO = codal::low(sp);
+}
+
+void tcb_configure_stack_base(void* tcb, PROCESSOR_WORD_TYPE stack_base)
+{
+    PROCESSOR_TCB* tcbPointer = (PROCESSOR_TCB *)tcb;
+    tcbPointer->stack_base = stack_base;
+}
+
+PROCESSOR_WORD_TYPE tcb_get_stack_base(void* tcb)
+{
+    return fiber_initial_stack_base();
+}
+
+PROCESSOR_WORD_TYPE get_current_sp()
+{
+    return SPH << 8 | SPL;;
+}
+
+PROCESSOR_WORD_TYPE tcb_get_sp(void* tcb)
+{
+    return tcb->SPHI << 8 | tcb->SPLO;
+}
+
+void tcb_configure_args(void* tcb, PROCESSOR_WORD_TYPE ep, PROCESSOR_WORD_TYPE cp, PROCESSOR_WORD_TYPE pm)
+{
+    PROCESSOR_TCB* tcbPointer = (PROCESSOR_TCB *)tcb;
+    // arg 3
+    tcbPointer->R20 = codal::low(pm);
+    tcbPointer->R21 = codal::high(pm);
+    // arg 2
+    tcbPointer->R22 = codal::low(cp);
+    tcbPointer->R23 = codal::high(cp);
+    // arg 1
+    tcbPointer->R24 = codal::low(ep);
+    tcbPointer->R25 = codal::high(ep);
+}
+
+
