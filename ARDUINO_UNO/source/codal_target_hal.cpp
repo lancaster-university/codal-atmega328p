9a701977cb89cd91a978bed4afe54d1d8ed5086c
James Devine <j.devine@lancaster.ac.uk>
2017-10-05 10:39:02 +0100

    Implemented the full codal_target_hal api
    Added fiber function definitions, operator new and delete, and the codal_heap_start symbol.

--- ARDUINO_UNO/source/codal_target_hal.cpp
+++ ARDUINO_UNO/source/codal_target_hal.cpp
@@ -5,6 +5,9 @@
 #include <avr/wdt.h>
 #include "ATMegaSerial.h"
 
+#include "CodalCompat.h"
+#include "ATMegaSerial.h"
+
 void target_enable_irq()
 {
     sei();
@@ -26,7 +29,8 @@ void target_wait_for_event()
 
 void target_wait(uint32_t milliseconds)
 {
-    _delay_ms(milliseconds);
+    while(milliseconds-- > 0)
+        _delay_ms(1);
 }
 
 void target_reset()
@@ -65,7 +69,7 @@ struct PROCESSOR_TCB
 
 PROCESSOR_WORD_TYPE fiber_initial_stack_base()
 {
-    return RAMEND - 0x02;;
+    return RAMEND - 0x02;
 }
 
 void* tcb_allocate()
@@ -82,7 +86,7 @@ void* tcb_allocate()
 void tcb_configure_lr(void* tcb, PROCESSOR_WORD_TYPE function)
 {
     PROCESSOR_TCB* tcbPointer = (PROCESSOR_TCB *)tcb;
-    tcbPointer-LR = function;
+    tcbPointer->LR = function;
 }
 
 /**
@@ -94,14 +98,15 @@ void tcb_configure_lr(void* tcb, PROCESSOR_WORD_TYPE function)
 void tcb_configure_sp(void* tcb, PROCESSOR_WORD_TYPE sp)
 {
     PROCESSOR_TCB* tcbPointer = (PROCESSOR_TCB *)tcb;
-    tcb->SPHI = codal::high(sp);
-    tcb->SPLO = codal::low(sp);
+    tcbPointer->SPHI = codal::high(sp);
+    tcbPointer->SPLO = codal::low(sp);
 }
 
 void tcb_configure_stack_base(void* tcb, PROCESSOR_WORD_TYPE stack_base)
 {
     PROCESSOR_TCB* tcbPointer = (PROCESSOR_TCB *)tcb;
-    tcbPointer->stack_base = stack_base;
+    tcbPointer->SPHI = codal::high(stack_base);
+    tcbPointer->SPLO = codal::low(stack_base);
 }
 
 PROCESSOR_WORD_TYPE tcb_get_stack_base(void* tcb)
@@ -111,12 +116,13 @@ PROCESSOR_WORD_TYPE tcb_get_stack_base(void* tcb)
 
 PROCESSOR_WORD_TYPE get_current_sp()
 {
-    return SPH << 8 | SPL;;
+    return SPH << 8 | SPL;
 }
 
 PROCESSOR_WORD_TYPE tcb_get_sp(void* tcb)
 {
-    return tcb->SPHI << 8 | tcb->SPLO;
+    PROCESSOR_TCB* tcbPointer = (PROCESSOR_TCB *)tcb;
+    return tcbPointer->SPHI << 8 | tcbPointer->SPLO;
 }
 
 void tcb_configure_args(void* tcb, PROCESSOR_WORD_TYPE ep, PROCESSOR_WORD_TYPE cp, PROCESSOR_WORD_TYPE pm)
@@ -134,3 +140,19 @@ void tcb_configure_args(void* tcb, PROCESSOR_WORD_TYPE ep, PROCESSOR_WORD_TYPE c
 }
 
 
+extern "C" void __cxa_pure_virtual()
+{
+    target_panic(1000);
+}
+
+extern PROCESSOR_WORD_TYPE __heap_start;
+PROCESSOR_WORD_TYPE codal_heap_start = (PROCESSOR_WORD_TYPE)&__heap_start - ((PROCESSOR_WORD_TYPE)&__heap_start % (PROCESSOR_WORD_TYPE)DEVICE_HEAP_BLOCK_SIZE) + (PROCESSOR_WORD_TYPE)DEVICE_HEAP_BLOCK_SIZE;
+
+// define new and delete.
+extern "C" void* operator new(size_t objsize) {
+    return malloc(objsize);
+}
+
+extern "C" void operator delete(void* obj) {
+    free(obj);
+}
