3dd38df5cf607d7e434c0b301ae69ef2dbab81ee
Joe Finney <joe@comp.lancs.ac.uk>
2017-10-16 11:09:02 +0100

    Refactor I2C implementation to merge common funcitonality into codal-core
      - Remove duplicate funcitons with codal-core/source/drivers/I2C.cpp
      - Declare overriden funcitons as virtual

--- inc/ATMegaI2C.h
+++ inc/ATMegaI2C.h
@@ -33,9 +33,9 @@ namespace codal
 {
 
 /**
-  * Class definition for I2C service, derived from ARM mbed.
+  * Class definition for ATMega I2C device.
   */
-class ATMegaI2C
+class ATMegaI2C : I2C
 {
 public:
     ATMegaI2C(ATMegaPin &sda, ATMegaPin &scl);
@@ -44,17 +44,17 @@ public:
       *
       * @param frequency The bus frequency in hertz
       */
-    int setFrequency(uint32_t frequency);
+    virtual int setFrequency(uint32_t frequency);
 
     /**
      * Issues a START condition on the I2C bus
      */
-    int start();
+    virtual int start();
 
     /**
      * Issues a STOP condition on the I2C bus
      */
-    void stop();
+    virtual int stop();
 
     /**
       * Writes the given byte to the I2C bus.
@@ -64,7 +64,7 @@ public:
       * @param data The byte to write.
       * @return DEVICE_OK on success, DEVICE_I2C_ERROR if the the write request failed.
       */
-    int write(uint8_t data);
+    virtual int write(uint8_t data);
 
     /**
     * Reads a single byte from the I2C bus.
@@ -72,101 +72,8 @@ public:
     *
     * @return the byte read from the I2C bus, or DEVICE_I2C_ERROR if the the write request failed.
     */
-    int read();
+    virtual int read();
 
-    /**
-      * Issues a standard, 2 byte I2C command write to the I2C bus.
-      * This consists of:
-      *  - Asserting a Start condition on the bus
-      *  - Selecting the Slave address (as an 8 bit address)
-      *  - Writing the raw 8 bit data provided
-      *  - Asserting a Stop condition on the bus
-      *
-      * The CPU will busy wait until the transmission is complete.
-      *
-      * @param address The 8bit I2C address of the device to write to
-      * @param data the byte command to write
-      *
-      * @return DEVICE_OK on success, DEVICE_I2C_ERROR if the the write request failed.
-      */
-    int write(uint16_t address, uint8_t data);
-
-    /**
-    * Issues a standard, I2C command write to the I2C bus.
-    * This consists of:
-    *  - Asserting a Start condition on the bus
-    *  - Selecting the Slave address (as an 8 bit address)
-    *  - Writing a number of raw data bytes provided
-    *  - Asserting a Stop condition on the bus
-    *
-    * The CPU will busy wait until the transmission is complete.
-    *
-    * @param address The 8bit I2C address of the device to write to
-    * @param data pointer to the bytes to write
-    * @param len the number of bytes to write
-    *
-    * @return DEVICE_OK on success, DEVICE_I2C_ERROR if the the write request failed.
-    */
-    int write(uint16_t address, uint8_t *data, int len);
-
-    /**
-      * Performs a typical register write operation to the I2C slave device provided.
-      * This consists of:
-      *  - Asserting a Start condition on the bus
-      *  - Selecting the Slave address (as an 8 bit address)
-      *  - Writing the 8 bit register address provided
-      *  - Writing the 8 bit value provided
-      *  - Asserting a Stop condition on the bus
-      *
-      * The CPU will busy wait until the transmission is complete..
-      *
-      * @param address 8bit address of the device to write to
-      * @param reg The 8bit address of the register to write to.
-      * @param value The value to write.
-      *
-      * @return DEVICE_OK on success, DEVICE_I2C_ERROR if the the write request failed.
-      */
-    int writeRegister(uint16_t address, uint8_t reg, uint8_t value);
-
-    /**
-      * Issues a standard, 2 byte I2C command read to the I2C bus.
-      * This consists of:
-      *  - Asserting a Start condition on the bus
-      *  - Selecting the Slave address (as an 8 bit address)
-      *  - reading "len" bytes of raw 8 bit data into the buffer provided
-      *  - Asserting a Stop condition on the bus
-      *
-      * The CPU will busy wait until the transmission is complete.
-      *
-      * @param address The 8bit I2C address of the device to read from
-      * @param data pointer to store the the bytes read
-      * @param len the number of bytes to read into the buffer
-      *
-      * @return DEVICE_OK on success, DEVICE_I2C_ERROR if the the read request failed.
-      */
-    int read(uint16_t address, uint8_t *data, int len);
-
-    /**
-      * Performs a typical register read operation to the I2C slave device provided.
-      * This consists of:
-      *  - Asserting a Start condition on the bus
-      *  - Selecting the Slave address (as an 8 bit address, I2C WRITE)
-      *  - Selecting a RAM register address in the slave
-      *  - Asserting a Stop condition on the bus
-      *  - Asserting a Start condition on the bus
-      *  - Selecting the Slave address (as an 8 bit address, I2C READ)
-      *  - Performing an 8 bit read operation (of the requested register)
-      *  - Asserting a Stop condition on the bus
-      *
-      * The CPU will busy wait until the transmission is complete..
-      *
-      * @param address 8bit i2C address of the device to read from
-      * @param reg The 8bit register address of the to read.
-      *
-      * @return the value of the requested register, or DEVICE_I2C_ERROR if the the read request
-     * failed.
-      */
-    int readRegister(uint16_t address, uint8_t reg);
 };
 }
 
--- source/ATMegaI2C.cpp
+++ source/ATMegaI2C.cpp
@@ -42,9 +42,8 @@ namespace codal
 /**
   * Constructor.
   */
-ATMegaI2C::ATMegaI2C(ATMegaPin &sda, ATMegaPin &scl)
+ATMegaI2C::ATMegaI2C(ATMegaPin &sda, ATMegaPin &scl) : I2C(sda, scl)
 {
-    setFrequency(100000);
 }
 
 /** Set the frequency of the I2C interface
@@ -80,13 +79,14 @@ int ATMegaI2C::start()
 /**
  * Issues a STOP condition on the I2C bus
  */
-void ATMegaI2C::stop()
+int ATMegaI2C::stop()
 {
     // Initiate a stop condition
     TWCR = (1 << TWINT) | (1 << TWSTO) | (1 << TWEN);
 
     // Wait for start condition to be transmitted.
     // while (!TWI_DONE);
+    return DEVICE_OK;
 }
 
 /**
@@ -120,157 +120,4 @@ int ATMegaI2C::read()
     return TWDR;
 }
 
-/**
-  * Issues a standard, 2 byte I2C command write to the I2C bus.
-  * This consists of:
-  *  - Asserting a Start condition on the bus
-  *  - Selecting the Slave address (as an 8 bit address)
-  *  - Writing the raw 8 bit data provided
-  *  - Asserting a Stop condition on the bus
-  *
-  * The CPU will busy wait until the transmission is complete.
-  *
-  * @param address The 8bit I2C address of the device to write to
-  * @param data the byte command to write
-  *
-  * @return DEVICE_OK on success, DEVICE_I2C_ERROR if the the write request failed.
-  */
-  int ATMegaI2C::write(uint16_t address, uint8_t data)
-  {
-      return write(address, &data, 1);
-  }
-
-  /**
-  * Issues a standard, I2C command write to the I2C bus.
-  * This consists of:
-  *  - Asserting a Start condition on the bus
-  *  - Selecting the Slave address (as an 8 bit address)
-  *  - Writing a number of raw data bytes provided
-  *  - Asserting a Stop condition on the bus
-  *
-  * The CPU will busy wait until the transmission is complete.
-  *
-  * @param address The 8bit I2C address of the device to write to
-  * @param data pointer to the bytes to write
-  * @param len the number of bytes to write
-  *
-  * @return DEVICE_OK on success, DEVICE_I2C_ERROR if the the write request failed.
-  */
-  int ATMegaI2C::write(uint16_t address, uint8_t *data, int len)
-  {
-      if (data == NULL || len <= 0)
-          return DEVICE_INVALID_PARAMETER; // Send a start condition
-
-      start();
-
-      // Send the address of the slave, with a write bit set.
-      write((uint8_t)address);
-
-      // Send the body of the data
-      for (int i = 0; i < len; i++)
-          write(data[i]);
-
-      // Send a stop condition
-      stop();
-
-      return DEVICE_OK;
-  }
-
-  /**
-  * Performs a typical register write operation to the I2C slave device provided.
-  * This consists of:
-  *  - Asserting a Start condition on the bus
-  *  - Selecting the Slave address (as an 8 bit address)
-  *  - Writing the 8 bit register address provided
-  *  - Writing the 8 bit value provided
-  *  - Asserting a Stop condition on the bus
-  *
-  * The CPU will busy wait until the transmission is complete..
-  *
-  * @param address 8bit address of the device to write to
-  * @param reg The 8bit address of the register to write to.
-  * @param value The value to write.
-  *
-  * @return DEVICE_OK on success, DEVICE_I2C_ERROR if the the write request failed.
-  */
-  int ATMegaI2C::writeRegister(uint16_t address, uint8_t reg, uint8_t value)
-  {
-      uint8_t command[2];
-      command[0] = reg;
-      command[1] = value;
-
-      return write(address, command, 2);
-}
-
-/**
-* Issues a standard, 2 byte I2C command read to the I2C bus.
-* This consists of:
-*  - Asserting a Start condition on the bus
-*  - Selecting the Slave address (as an 8 bit address)
-*  - reading "len" bytes of raw 8 bit data into the buffer provided
-*  - Asserting a Stop condition on the bus
-*
-* The CPU will busy wait until the transmission is complete.
-*
-* @param address The 8bit I2C address of the device to read from
-* @param data pointer to store the the bytes read
-* @param len the number of bytes to read into the buffer
-*
-* @return DEVICE_OK on success, DEVICE_I2C_ERROR if the the read request failed.
-*/
-int ATMegaI2C::read(uint16_t address, uint8_t *data, int len)
-{
-    if (data == NULL || len <= 0)
-        return DEVICE_INVALID_PARAMETER;
-
-    // Send a start condition
-    start();
-
-    // Send the address of the slave, with a read bit set.
-    write((uint8_t)(address | 0x01));
-
-    // Read the body of the data
-    for (int i = 0; i < len; i++)
-        data[i] = read();
-
-    // Send a stop condition
-    stop();
-
-    return DEVICE_OK;
-}
-
-/**
-* Performs a typical register read operation to the I2C slave device provided.
-* This consists of:
-*  - Asserting a Start condition on the bus
-*  - Selecting the Slave address (as an 8 bit address, I2C WRITE)
-*  - Selecting a RAM register address in the slave
-*  - Asserting a Stop condition on the bus
-*  - Asserting a Start condition on the bus
-*  - Selecting the Slave address (as an 8 bit address, I2C READ)
-*  - Performing an 8 bit read operation (of the requested register)
-*  - Asserting a Stop condition on the bus
-*
-* The CPU will busy wait until the transmission is complete..
-*
-* @param address 8bit i2C address of the device to read from
-* @param reg The 8bit register address of the to read.
-*
-* @return the value of the requested register, or DEVICE_I2C_ERROR if the the read request failed.
-*/
-int ATMegaI2C::readRegister(uint16_t address, uint8_t reg)
-{
-    int result;
-    uint8_t data;
-
-    result = write(address, reg);
-    if (result != DEVICE_OK)
-        return result;
-
-    result = read(address, &data, 1);
-    if (result != DEVICE_OK)
-        return result;
-
-    return data;
-}
 }
